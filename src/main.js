971d8dbc15769aca88408d9f3bbae213864c1d20
Lin <hailin_zhu2018@126.com>
2021-09-21 20:53:16 +0800

    echarts移动端适配

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import * as echarts from 'echarts';
 const main = document.getElementById('main');
 const loadMoreButton = document.getElementById('loadMore');
 
+const width = document.documentElement.clientWidth;
+main.style.width = `${width}px`;
+main.style.height = `${width*1.2}px`
+
 const myChart = echarts.init(main,'default');
 let n = 0;
 let m=0;
@@ -18,37 +22,52 @@ let xData = [createKey(),createKey(),createKey(),createKey(),createKey(),createK
 let values = [createValue(),createValue(),createValue(),createValue(),createValue(),createValue(),createValue()]
 // 使用刚指定的配置项和数据显示图表。
 myChart.setOption({
-    title:{
-        show:true,
-        text:'bug数',
-        right:0,
+    baseOption:{
+        title:{
+            show:true,
+            text:'bug数',
+            right:20,
+        },
+        legend: {
+            data:['bug数']
+        },
+        xAxis: {
+            type: 'category',
+            data: xData,
+        },
+        yAxis: {
+            type: 'value'
+        },
+        tooltip:{
+            show:true,
+        },
+        series: [
+            {
+                name:'bug数',
+                data: values,
+                type: 'line',
+                lineStyle:{
+                    color:'red',
+                },
+            }
+        ]
     },
-    legend: {
-        data:['bug数']
-    },
-    xAxis: {
-        type: 'category',
-        data: xData,
-    },
-    yAxis: {
-        type: 'value'
-    },
-    tooltip:{
-        show:true,
-    },
-    series: [
-        {
-            lineStyle:{
-                color:'red',
-            },
-            itemStyle:{
-                color:"blue",
-            },
-            name:'bug数',
-            data: values,
-            type: 'line'
+    media:[{
+        query:{
+            maxWidth:500,
+        },
+        option:{
+            series: [
+                {
+                   itemStyle:{
+                       borderWidth:30
+                   }
+                }
+            ]
         }
-    ]
+
+    }]
+
 });
 let isLoading = false;
 loadMoreButton.addEventListener('click',()=>{
